7befcb955e2ee628a592fe8e9a0e944380ca0e77
Windows <Windows@WINDOWS-PC>
2026-04-09 15:29:08 +0700

    Fix login validation Update UI home page

--- DoAnTotNghiep/Controllers/HomeController.cs
+++ DoAnTotNghiep/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DoAnTotNghiep.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -6,44 +7,68 @@ using System.Web.Mvc;
 
 namespace DoAnTotNghiep.Controllers
 {
+    
     public class HomeController : Controller
     {
+        private Model1 db = new Model1();
+        private void LoadMenu()
+        {
+            ViewBag.DanhMucMenu = db.DanhMuc.ToList();
+        }
         public ActionResult TrangChu()
         {
+            LoadMenu();
             return View();
         }
         public ActionResult GioiThieu()
         {
+            LoadMenu();
             return View();
         }
         public ActionResult sanpham()
         {
+            LoadMenu();
             return View();
         }
         public ActionResult CSTT()
         {
+            LoadMenu();
             return View();
         }
         public ActionResult CSQRT()
         {
+            LoadMenu();
             return View();
         }
         public ActionResult CSMH()
         {
+            LoadMenu();
             return View();
         }
         public ActionResult CSGH()
         {
+            LoadMenu();
             return View();
         }
         public ActionResult CSDT()
         {
+            LoadMenu();
             return View();
         }
         public ActionResult LienHe()
         {
+            LoadMenu();
             return View();
         }
-      
+        public ActionResult TinTuc()
+        {
+            LoadMenu();
+            return View();
+        }
+        public ActionResult GioHang()
+        {
+            LoadMenu();
+            return View();
+        }
     }
 }
